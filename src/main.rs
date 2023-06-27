4b2f9a325afd42e68a04e3098376e253dca859b2
Dmitry Kuznetsov <dev.kuznetsov.d@gmail.com>
2023-06-27 23:10:24 +0200

    Make working merger for specific options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -29,5 +30,40 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    println!("{:?}", args);
+    // println!("{:?}", args);
+
+    // ID - [0]
+    // UUID - [1]
+    let mut file1 = csv::Reader::from_path(args.file1_path).expect("Must be a CSV file.");
+
+    // Payment ID - [0]
+    // Title - [1]
+    let mut file2 = csv::Reader::from_path(args.file2_path).expect("Must be a CSV file.");
+
+    let mut content = HashMap::new();
+
+    for record in file2.records() {
+        let row = record.expect("a CSV record");
+
+        let payment_id = row.get(0).expect("a valid UTF-8 string").to_string();
+        let title = row.get(1).expect("a valid UTF-8 string").to_string();
+
+        content.insert(payment_id, title);
+    }
+
+    let mut writer = csv::Writer::from_path("result.csv").expect("some writer");
+
+    for record in file1.records() {
+        let row = record.expect("a CSV record");
+
+        let payment_id = row.get(0).expect("a valid UTF-8 string").to_string();
+        let uuid = row.get(1).expect("a valid UTF-8 string").to_string();
+
+        let title = match content.get(&payment_id) {
+            Some(title) => title.to_string(),
+            None => continue,
+        };
+
+        writer.write_record(&[uuid, title]).expect("writing error");
+    }
 }
