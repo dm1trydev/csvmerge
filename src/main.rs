2e73532b1e46300b3a2a99bdb8b246b436041e36
Dmitry Kuznetsov <dev.kuznetsov.d@gmail.com>
2023-07-18 22:00:31 +0200

    Replace old columns retrieve with MergeField

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
+mod merge_field;
+
 use clap::Parser;
 use std::collections::HashMap;
+use merge_field::MergeField;
+
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Формат разделителей первого и второго csv файлов соответственно
-    #[arg(short, long, default_value_t = String::from(","))]
-    format: String,
+    #[arg(short = 'D', long, default_value_t = String::from(",,"))]
+    delimeter: String,
 
-    /// Флаг обозначающий наличие или отсутствие заголовков в файлах
-    #[arg(short, long, default_value_t = true)]
-    no_headers: bool,
+    /// Название или номер столбца, для поиска в первом файле
+    #[arg(short, long, value_parser = MergeField::parse)]
+    source: MergeField, // replace with MergeField
 
-    /// название столбца, для поиска соответствий в обоих файлах.
-    /// Может принять также позиции столбцов в обоих файлах через `$1`, `$2`, где `$1` - первый столбец первого файла,
-    /// `$2` -  второй столбец второго файла.
-    #[arg(short, long, default_value_t = String::from("$1"))]
-    source: String,
-
-    /// список полей, которые необходимо взять из первого и второго файлов соответственно.
-    /// Разделяются запятой между собой и пробелом между файлами
-    #[arg(short, long, default_value_t = String::from("$1"))]
-    destination: String,
+    /// Название или номер столбца, для поиска во втором файле
+    #[arg(short, long, value_parser = MergeField::parse)]
+    destination: MergeField,
 
     file1_path: std::path::PathBuf,
     file2_path: std::path::PathBuf,
@@ -29,8 +26,6 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
-
-    // println!("{:?}", args);
 
     // ID - [0]
     // UUID - [1]
@@ -40,13 +35,23 @@ fn main() {
     // Title - [1]
     let mut file2 = csv::Reader::from_path(args.file2_path).expect("Must be a CSV file.");
 
+    println!("{:?}", args.source);
+    println!("{:?}", args.destination);
+
     let mut content = HashMap::new();
 
     for record in file2.records() {
         let row = record.expect("a CSV record");
 
-        let payment_id = row.get(0).expect("a valid UTF-8 string").to_string();
-        let title = row.get(1).expect("a valid UTF-8 string").to_string();
+        let payment_id = match args.source {
+            MergeField::Number(number) => row.get(number).expect("a valid UTF-8 string").to_string(),
+            _ => todo!(),
+        };
+
+        let title = match args.destination {
+            MergeField::Number(number) => row.get(number).expect("a valid UTF-8 string").to_string(),
+            _ => todo!(),
+        };
 
         content.insert(payment_id, title);
     }
