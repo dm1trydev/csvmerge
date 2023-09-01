8e85180092b7541d061bfff737d92bd7d98bb5db
Dmitry Kuznetsov <dev.kuznetsov.d@gmail.com>
2023-09-01 23:32:05 +0200

    Use MergeField in ResultFields

--- src/result_fields.rs
+++ src/result_fields.rs
@@ -1,34 +1,30 @@
-#[derive(Debug)]
-enum Column {
-    Number(usize),
-    Name(String),
-}
+use super::merge_field::MergeField;
 
 #[derive(Debug)]
 pub struct ResultFields {
-    file1: Vec<Column>,
-    file2: Vec<Column>,
+    file1: Vec<MergeField>,
+    file2: Vec<MergeField>,
 }
 
 impl ResultFields {
     pub fn new(s: &str) -> Self {
-        let mut files: Vec<Vec<Column>> = s.split(' ').map(|file| Self::parse_columns(file)).collect();
+        let mut files: Vec<Vec<MergeField>> = s.split(' ').map(|file| Self::parse_columns(file)).collect();
         let file2 = files.pop().unwrap();
         let file1 = files.pop().unwrap();
 
         Self { file1, file2 }
     }
 
-    fn parse_columns(s: &str) -> Vec<Column> {
-        let mut result: Vec<Column> = Vec::new();
+    fn parse_columns(s: &str) -> Vec<MergeField> {
+        let mut result: Vec<MergeField> = Vec::new();
 
         for column in s.split(',') {
             let number = column.parse::<usize>();
 
             if number.is_ok() {
-                result.push(Column::Number(number.unwrap()));
+                result.push(MergeField::Number(number.unwrap()));
             } else {
-                result.push(Column::Name(column.to_string()));
+                result.push(MergeField::Name(column.to_string()));
             }
         }
 
@@ -47,8 +43,8 @@ mod tests {
     #[test]
     fn it_creates_source_with_field_number() {
         let result = ResultFields::new("0,1,2 0,2,3");
-        let expected_file1 = vec![Column::Number(0), Column::Number(1), Column::Number(2)];
-        let expected_file2 = vec![Column::Number(0), Column::Number(2), Column::Number(3)];
+        let expected_file1 = vec![MergeField::Number(0), MergeField::Number(1), MergeField::Number(2)];
+        let expected_file2 = vec![MergeField::Number(0), MergeField::Number(2), MergeField::Number(3)];
 
         assert_eq!(compare_vecs(&result.file1, &expected_file1), true);
         assert_eq!(compare_vecs(&result.file2, &expected_file2), true);
@@ -57,14 +53,14 @@ mod tests {
     #[test]
     fn it_creates_source_with_field_name() {
         let result = ResultFields::new("uuid,title name,score");
-        let expected_file1 = vec![Column::Name("uuid".to_string()), Column::Name("title".to_string())];
-        let expected_file2 = vec![Column::Name("name".to_string()), Column::Name("score".to_string())];
+        let expected_file1 = vec![MergeField::Name("uuid".to_string()), MergeField::Name("title".to_string())];
+        let expected_file2 = vec![MergeField::Name("name".to_string()), MergeField::Name("score".to_string())];
 
         assert_eq!(compare_vecs(&result.file1, &expected_file1), true);
         assert_eq!(compare_vecs(&result.file2, &expected_file2), true);
     }
 
-    fn compare_vecs(a: &Vec<Column>, b: &Vec<Column>) -> bool {
+    fn compare_vecs(a: &Vec<MergeField>, b: &Vec<MergeField>) -> bool {
         if a.len() != b.len() {
             return false;
         }
@@ -73,9 +69,9 @@ mod tests {
 
         for i in 0..a.len() {
             match &a[i] {
-                Column::Number(first) => {
+                MergeField::Number(first) => {
                     match &b[i] {
-                        Column::Number(second) => {
+                        MergeField::Number(second) => {
                             if first != second {
                                 result = false;
                             }
@@ -83,9 +79,9 @@ mod tests {
                         _ => { result = false },
                     }
                 },
-                Column::Name(first) => {
+                MergeField::Name(first) => {
                     match &b[i] {
-                        Column::Name(second) => {
+                        MergeField::Name(second) => {
                             if first != second {
                                 result = false;
                             }
